cf2537baf9e61ba74cc9088b51de708c1154acb3
tlenoir7 <tylerlenoir07@gmail.com>
2026-03-24 21:10:07 -0400

    fix: run.py point to asgi_app not app

--- run.py
+++ run.py
@@ -3,4 +3,4 @@ import uvicorn
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
-    uvicorn.run("web_app_fastapi:app", host="0.0.0.0", port=port)
+    uvicorn.run("web_app_fastapi:asgi_app", host="0.0.0.0", port=port)
